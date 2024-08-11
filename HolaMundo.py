923939eb8d6f52be6b52d28a76abfb0f889f7ba8
josemmurillas <jmurillas99@gmail.com>
2024-08-11 12:39:37 -0500

    Primer archivo hola mundo

---
+++ HolaMundo.py
@@ -0,0 +1,46 @@
+print("hola mundo")
+
+#tipo de datos enteros
+a = 10
+b = 23
+print(a+b) 
+
+#los números enteros en formato binario, octal o hexadecima
+cinco = 5
+cincoBinario = 0b101
+cincoOctal = 0o5
+cincoHexa = 0x5 
+
+print(cincoHexa)
+print(cincoOctal)
+print(cincoBinario)
+
+#Número decimales
+real = 1.1 + 2.2 #una operacion decimal 
+unReal = 1.3 #una variable decimal
+otroReal = 1/2 #un resultado decimal
+notClient = 1.2E8 #Notacion cientifica 
+print(notClient)
+print(otroReal)
+print(real)
+print(f'{real:.1f}')#se utiliza la funcion f'' para aproximizar el numero a 1 decimal o el que se quiera
+
+#Números complejos tienen una parte real y otra imaginaria
+complejo = 1+2j
+print(complejo.imag)
+print(complejo.real)
+
+#Númerios aritmeticos
+suma = 1 + 2.3 #Suma decimales
+resta = 3 - 1.5 #Resta decimales  
+multiplicacion = 2 * 5.5 #Multiplicacion con resultado decimal
+division = 2 / 3 #Division
+modu = 15 % 20 #el resto de la division
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(division)
+print(modu)
+
+#tipo de datos booleanos
