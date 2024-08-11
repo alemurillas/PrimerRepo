1f458370d190684805679159d18ffce278c26403
josemmurillas <jmurillas99@gmail.com>
2024-08-11 12:57:28 -0500

    Archivos python

---
+++ lista.py
@@ -0,0 +1,7 @@
+#Lista
+ciudad =['buenos aires', 'cali', 'medellin', 'santiago de chile']
+print (ciudad[3])
+
+#Diccionario
+animales = {"pez":"pez espada", "anfibio":"rana", "reptil":"cocodrilo","ave":"loro", "mamifero":"elefante" }
+print(animales["pez"], animales["mamifero"]) 
